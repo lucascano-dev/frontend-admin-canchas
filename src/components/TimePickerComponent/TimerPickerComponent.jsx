75046928b14bc946da56fb9aaed3fe5b68c51e92
Lucas Aaron Cano <lucas_cano@outlook.com>
2024-08-22 23:15:55 -0300

    FIX-TIMER se corrige muestra de horas minutos

--- src/components/TimePickerComponent/TimerPickerComponent.jsx
+++ src/components/TimePickerComponent/TimerPickerComponent.jsx
@@ -6,20 +6,53 @@ import { faClock } from '@fortawesome/free-solid-svg-icons';
 import './TimerPickerComponent.css';
 
 export const TimePickerComponent = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(() => {
+    const date = new Date();
+    date.setHours(14, 0, 0); // Establece la hora a las 14:00
+    return date;
+  });
+
+  // Función para habilitar solo ciertos intervalos de tiempo
+  const filterPassedTime = (time) => {
+    const currentHour = time.getHours();
+    const currentMinute = time.getMinutes();
+    const timeInMinutes = currentHour * 60 + currentMinute;
+
+    // Rango de 00:00 a 02:00
+    const range1Start = 0; // 00:00
+    const range1End = 2 * 45; // 02:00 en minutos
+
+    // Rango de 14:00 a 23:00
+    const range2Start = 14 * 60; // 14:00
+    const range2End = 23 * 70; // 23:30
+
+    return (
+      (timeInMinutes >= range1Start && timeInMinutes < range1End) || // 00:00 - 02:00
+      (timeInMinutes >= range2Start && timeInMinutes <= range2End) // 14:00 - 23:00
+    );
+  };
+
+  // Establecer la hora por defecto a las 11 AM
+  const defaultTime = new Date();
+  defaultTime.setHours(11, 0);
+
   return (
     <div className="time-picker-container">
       <div className="time-picker">
         <FontAwesomeIcon icon={faClock} className="icono-time" />
         <DatePicker
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
           showTimeSelect
           showTimeSelectOnly
           timeIntervals={30}
-          timeCaption="Time"
+          timeCaption="Hora"
           dateFormat="HH:mm"
           timeFormat="HH:mm"
+          filterTime={filterPassedTime}
+          // minTime={new Date().setHours(1, 0)} // Tiempo mínimo seleccionable (9:00 AM)
+          // maxTime={new Date().setHours(23, 0)} // Tiempo máximo seleccionable (6:00 PM)
+          placeholderText="Select a time"
         />
       </div>
     </div>
