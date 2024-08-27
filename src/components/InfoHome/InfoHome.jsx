a7ec54d807e8bdf70937f9bfa328d5a834382609
Lucas Aaron Cano <lucas_cano@outlook.com>
2024-08-27 13:33:52 -0300

    FE-SECCIONINFO se agregan secciones y se acomodan cards con map

---
+++ src/components/InfoHome/InfoHome.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './InfoHome.css';
+
+export const InfoHome = () => {
+  return <div className="InfoHome">InfoHome</div>;
+};
---
+++ src/components/InfoPublicidad/InfoPublicidad.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const InfoPublicidad = () => {
+  return <div>InfoPublicidad</div>;
+};
