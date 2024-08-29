563ec27c8370708f77708eee8f00e0f0c7ff7f08
Lucas Aaron Cano <lucas_cano@outlook.com>
2024-08-28 22:01:22 -0300

    FE-FECHAHORA-ISO8601 guardar datos de turno en localStorage

--- src/components/TurneroSelector/TurneroSelector.jsx
+++ src/components/TurneroSelector/TurneroSelector.jsx
@@ -18,6 +18,8 @@ export const TurneroSelector = () => {
       console.log('Fecha seleccionada:', selectedDate);
       console.log('Hora seleccionada:', selectedTime);
       console.log('Formato ISO', selectedDate + 'T' + selectedTime + ':00-03:00'); //Doy formato hora ISO 8601
+      const fechaHoraISO8601 = selectedDate + 'T' + selectedTime + ':00-03:00';
+      localStorage.setItem('turnoCanchaSeleccionado', fechaHoraISO8601);
     } else {
       alert('Por favor, selecciona una fecha y una hora');
     }
